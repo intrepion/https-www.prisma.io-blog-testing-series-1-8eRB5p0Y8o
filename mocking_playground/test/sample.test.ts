ac69a2ba4bbea1092539b753b93356ce66732f44
Oliver Forral <intrepion@gmail.com>
2024-06-20 13:37:20 -0600

    Mock Prisma Client

--- mocking_playground/test/sample.test.ts
+++ mocking_playground/test/sample.test.ts
@@ -1,9 +1,13 @@
 //test/sample.test.ts
-import { expect, test } from "vitest";
+import { expect, test, vi } from 'vitest' // 👈🏻 Added the `vi` import
 import { createUser } from "../script";
+import prisma from '../libs/__mocks__/prisma'
+
+vi.mock('../libs/prisma')
 
 test("createUser should return the generated user", async () => {
   const newUser = { email: "user@prisma.io", name: "Prisma Fan" };
+  prisma.user.create.mockResolvedValue({ ...newUser, id: 1 })
   const user = await createUser(newUser);
   expect(user).toStrictEqual({ ...newUser, id: 1 });
 });
